4ccc519856ac21a696833a0904f3b23cef029fe9
Alex <25893802+alli2191@users.noreply.github.com>
2017-12-11 22:19:48 +1000

    Fully functioning purge function :D

--- ChaosBot.js
+++ ChaosBot.js
@@ -8,24 +8,18 @@ client.on("ready", () => {
 
 client.on("message", (message) => {
 
-// Reactions to James' bot
   if (message.author.id === config.Rival) {
-    message.react('👎');
-
-    setTimeout( function(){
-      message.react('🇮');
-    }, 500);
-
-    setTimeout( function(){
-      message.react('🔪');
-    }, 1000);
-
-    setTimeout( function(){
-      message.react('🇺');
-    }, 1500);
+    let i = 50; let incr = 500;
+    setTimeout( function(){message.react('👎');}, i);
+    i += incr;
+    setTimeout( function(){message.react('🇮');}, i);
+    i += incr;
+    setTimeout( function(){message.react('🔪');}, i);
+    i += incr;
+    setTimeout( function(){message.react('🇺');}, i);
   }
 
-// Ignore self and bots
+// Prevent bot-ception
   if (message.author.bot) return;
 
 // Anything after this requires the prefix to respond
@@ -33,8 +27,8 @@ client.on("message", (message) => {
 
 // Don't respond to James
   if(message.author.id === config.James) {
-    message.channel.send(`go away <@${config.James}>`)
-    return
+    message.channel.send(`go away <@${config.James}>. just kidding.`)
+    //return
   }
 
 // Bot Commands
@@ -43,7 +37,7 @@ const command = args.shift().toLowerCase();
 
   if (command === "help") {
     var helptext = `HELP: The prefix is \" ${config.prefix} \" ` +
-    "```\nValid Commands:\n\nping\ngoodbot```";
+    "```\nValid Commands:\n\nping\ngoodbot\npurge @name (optional) n\n```";
     message.channel.send(helptext)
   }
 
@@ -53,11 +47,63 @@ const command = args.shift().toLowerCase();
 
   if (command === "goodbot") {
     message.channel.send(`Thanks <@${message.author.id}>! 😇`);
+    console.log('Complimented!');
   }
 
-  if (message.author.id === config.ownerID) {
-    message.channel.send(`oi <@${config.ownerID}>`)
-  }
+// Start Purge
+    if (command === "purge") {
+
+      if(message.author.id === config.ownerID || message.author.id === config.James || message.author.id === config.Jesse) {
+
+        var excessDeletion = false;
+        const user = message.mentions.users.first();
+        let amount = parseInt(args[args.length - 1],10);
+
+        // Invalid input errors
+        if (isNaN(amount) || amount < 2) return message.reply('Must specify an amount (2-50) to delete!');
+
+        if (user.id === message.author.id) {amount = amount + 1;}
+        console.log(`Preparing to delete ${amount} messages (including sent message)`);
+
+        // Prevent excess deletion
+        if (amount > 51) {excessDeletion = true;}
+        if (excessDeletion === true) {
+          amount = 51;
+          console.log("Reduced to 51 (actually 50)");
+        }
+
+        // If User Specified
+        if (user) {
+            message.channel.fetchMessages({
+              limit: 100,
+            }).then((messages) => {
+               const filterBy = user ? user.id : Client.user.id;
+               messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
+               if (messages.length >= 2) {
+                 message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
+                } else return message.reply(' not enough messages from specified user')
+            });
+          } else {
+        // If No User Specified
+            message.channel.fetchMessages({
+              limit: amount,
+            }).then((messages) => {
+               message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
+            });
+          }
+        // Warning About Being Too Keen
+          if (excessDeletion === true) {
+            message.channel.send('Too big, reduced to 50.');
+          }
+          excessDeletion = false;
+      } else return message.reply('Not Authorized To Purge!');
+    }
+// End Purge
+
+// Debug by oi'ing
+  //if (message.author.id === config.ownerID) {
+  //  message.channel.send(`oi <@${config.ownerID}>`)
+  //}
 
 });
 

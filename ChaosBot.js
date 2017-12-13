3a74499812ed5bfb696ebcae34381c067d28fb6c
JamesBacon <JamesBacon@users.noreply.github.com>
2017-12-13 13:12:46 +1030

    The Poll. Use it or not.
    I saw you were using poll.js and was like, why. So here's one that works all within Discord.js.
    It was coding for the master branch of Discord.js so you should have to change too many lines to make it work on stable.

--- ChaosBot.js
+++ ChaosBot.js
@@ -7,18 +7,52 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+  if(msg.content.startsWith(prefix + "poll")) {
+      let i = 0; let I = 500;
+      var defaultVoteTime = 60000;
+      let pollCreator = msg.author.username;
+      let messageContent = msg.content.slice(5);
+      let getTime = msg.content.slice(-1)[0];
+      let voteTime = getTime * defaultVoteTime;
+      const embed = new Discord.MessageEmbed()
+      .setColor('#27ae60')
+      .addField("Poll Posted by: " + pollCreator,
+      messageContent, false)
+      .setFooter("You have " + getTime + " minute(s) to vote. Hurry!")
+      msg.channel.send({embed}).then(function (reply) {
+        // Adds the reactions the the message
+        setTimeout( function() { reply.react('👍'); }, i, i+=I);
+        setTimeout( function() { reply.react('👎'); }, i, i+=I);
 
-  if (message.content.startsWith(config.prefix + "ping")) {
-    message.channel.send("pong!");
-  } else
+        // The Yes vote.
+        var collecterYesUsers;
+        const collectorYes = reply.createReactionCollector(
+          (reaction, user) => reaction.emoji.name === '👍', { time: voteTime }
+        );
+        collectorYes.on('collect', r => console.log(r.emoji.name));
+        collectorYes.on('end', collected => collecterYesUsers = collectorYes.users.array());
 
-  if (message.content.startsWith(config.prefix + "foo")) {
-    message.channel.send("bar!");
-  }
-  if(message.author.id == "376906570296197123") {
-    message.react("❤");
-  }
+        // The No vote.
+        var collecterNoUsers;
+        const collectorNo = reply.createReactionCollector(
+          (reaction, user) => reaction.emoji.name === '👎', { time: voteTime }
+        );
+        collectorNo.on('collect', r => console.log(r.emoji.name));
+        collectorNo.on('end', collected => collecterNoUsers = collectorNo.users.array());
+
+        // The final Poll Results
+        setTimeout( function() {
+          const embed = new Discord.MessageEmbed()
+          .setColor('#c0392b')
+          .addField("Poll is now over!",
+          messageContent)
+          .addField("Yes Votes: ", collecterYesUsers.length -1, true)
+          .addField("No Votes: ", collecterNoUsers.length -1, true)
+          .setFooter("Poll is now over")
+          msg.channel.send({embed})
+        }, voteTime);
+      })
+    }
 });
 
 client.login(config.token);
